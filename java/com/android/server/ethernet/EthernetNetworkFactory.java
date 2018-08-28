e4a78384e0a5f33a79e3aa8eb901ce4759ee2c8f
Weiguo Hu <hwg@rock-chips.com>
2018-08-28 11:31:59 +0800

    fix bug: Ethernet not reconnect when exchange connect mode
    Change-Id: I21b27270f6a3567b28c05050118c9fb9391605cb

--- java/com/android/server/ethernet/EthernetNetworkFactory.java
+++ java/com/android/server/ethernet/EthernetNetworkFactory.java
@@ -183,6 +183,7 @@ public class EthernetNetworkFactory extends NetworkFactory {
         NetworkInterfaceState network = mTrackingInterfaces.get(iface);
         if (network != null) {
             network.setIpConfig(ipConfiguration);
+            network.reconnect();
         }
     }
 
@@ -369,6 +370,14 @@ public class EthernetNetworkFactory extends NetworkFactory {
                 mNetworkAgent = null;
             }
             clear();
+        }
+
+        void reconnect() {
+            updateLinkState(false);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignore) {}
+            updateLinkState(true);
         }
 
         private void updateAgent() {
